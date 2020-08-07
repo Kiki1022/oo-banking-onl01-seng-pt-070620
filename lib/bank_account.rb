1d7185f2381eed204be8b4be10c2a475e55e070c
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-07 03:54:40 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class BankAccount
-
+  
+ attr_reader :name, :balance, :status
+ 
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    #@broke = 0
+  end
+  
+  def deposit(money)
+    @balance = 2000
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    #binding.pry
+    @status = open && @balance > 0
+  end
 end
